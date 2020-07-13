99c20994e8363b5b8afa6073731d37a4ef347639
liyongping <liyongping@ccx.cn>
2020-07-13 11:22:58 +0800

    escredit-base-boot: 文档类swagger开关

--- escredit-base-boot/src/main/java/com/escredit/base/boot/aop/log/LogAspect.java
+++ escredit-base-boot/src/main/java/com/escredit/base/boot/aop/log/LogAspect.java
@@ -13,8 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
