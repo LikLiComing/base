e6ed269fa38bcb8eca61c8d84f6e88e2c90bcb38
liyongping <liyongping@ccx.cn>
2020-07-21 19:06:23 +0800

    rm:escredit-base-boot:去掉多余引入

---
+++ escredit-base-rules/src/test/java/com/escredit/base/rules/test/RulesTest.java
@@ -0,0 +1,27 @@
+//package com.escredit.base.rules.test;
+//
+//import junit.framework.TestCase;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.kie.api.runtime.KieContainer;
+//import org.kie.api.runtime.StatelessKieSession;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//import org.springframework.test.context.web.WebAppConfiguration;
+//
+//@RunWith(SpringRunner.class)
+//@WebAppConfiguration
+//@SpringBootTest
+//public class RulesTest extends TestCase {
+//
+//    @Autowired
+//    private KieContainer kieContainer;
+//
+//    @Test
+//    public void test1(){
+//        StatelessKieSession statelessKieSession = kieContainer.newStatelessKieSession("");
+//
+//        statelessKieSession.execute("aaa");
+//    }
+//}
