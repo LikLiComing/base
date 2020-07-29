e4c238d0f17d16f0a681c4fd54cabd146bf801b6
chenhao <chenhao@ccx.cn>
2020-07-29 17:25:13 +0800

    Refactor:email:修改邮件模块

---
+++ escredit-base-email/src/main/java/com/escredit/base/email/entity/BaseMailConstant.java
@@ -0,0 +1,9 @@
+package com.escredit.base.email.entity;
+
+/**
+ * @author ChH
+ * @create 2020/7/29
+ */
+public class BaseMailConstant {
+    public static final String CONSULT_PROGRAM = "consultProgram.ftl";
+}
